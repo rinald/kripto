05ab57b430bc49af0c95cff2151c04d8880cf8ec
Rinald <92527147+rinald-ta@users.noreply.github.com>
2023-04-09 12:05:00 +0000

    Update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
-export default function Home() {
-  return <main>Hello world</main>
+import Link from 'next/link'
+
+import { Asset } from './coin/[id]/types'
+
+const COINCAP_API_URL = 'https://api.coincap.io/v2'
+
+export default async function Home() {
+  const coins: { data: Asset[] } = await fetch(
+    `${COINCAP_API_URL}/assets?limit=2000`
+  ).then(res => res.json())
+
+  return (
+    <main style={{ marginLeft: 20 }}>
+      <ul>
+        {coins.data.map(coin => (
+          <li key={coin.id}>
+            <Link href={`/coin/${coin.id}`}>{coin.id}</Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
 }
