e9414357c79351797b769aa0868e31211e3e54de
Rinald Shabani <rinaldshabani@gmail.com>
2023-04-16 16:51:57 +0000

    Deploy to edge

--- sst.config.ts
+++ sst.config.ts
@@ -10,7 +10,9 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const site = new NextjsSite(stack, "site", {
+        edge: true,
+      });
 
       stack.addOutputs({
         SiteUrl: site.url,
